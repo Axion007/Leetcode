ae6c780bfd96eaa6368e41b2b8657c6aac76053f
Axion007 <151603573+Axion007@users.noreply.github.com>
2026-08-25 08:58:43 +0000

    LeetCode Sync Runtime - 2 ms (99.33%), Memory - 47.5 MB (11.20%)

--- solutions/0001-two-sum/solution.java
+++ solutions/0001-two-sum/solution.java
@@ -1,18 +1,14 @@
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        for(int i = 0; i < nums.length; i++) {
-
-            for(int j = i + 1; j < nums.length; j++) {
-
-                if(target == nums[i] + nums[j]) {
-
-                    return new int[]{i, j};
-
-                }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compliment=target-nums[i];
+            if(map.containsKey(compliment)){
+                return new int[] { map.get(compliment), i };
             }
+            map.put(nums[i],i);
         }
-
-        return new int[]{};
+        return new int[] {};
     }
 }
